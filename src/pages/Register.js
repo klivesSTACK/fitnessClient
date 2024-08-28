377b42410953dde4abf407318e2fd19a5de8fc7e
klivesSTACK <larrybriones10@gmail.com>
2024-08-28 22:09:01 +0800

    fixing register

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -28,7 +28,7 @@ export default function Register() {
 
         e.preventDefault();
 
-        fetch('http://localhost:4000/users/register',{
+        fetch('https://fitnessapi-briones.onrender.com/users/register',{
 
         method: 'POST',
         headers: {
